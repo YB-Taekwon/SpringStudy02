310df20b1918fbd7e1a94d84195b1de808852433
YB-Taekwon <loundo001@gmail.com>
2025-03-21 00:32:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/ian/javatospring02/JavaToSpring02Application.java
@@ -0,0 +1,13 @@
+package com.ian.javatospring02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToSpring02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaToSpring02Application.class, args);
+    }
+
+}
